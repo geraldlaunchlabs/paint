4d2105c2ddc5a84f53418caeec1cf0e66afe3fb8
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-09-08 13:41:34 +0800

    project initialization

---
+++ Paint/Paint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Paint
+//
+//  Created by LLDM on 08/09/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
